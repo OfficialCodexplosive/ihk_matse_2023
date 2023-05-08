26101af04a6355a4a2b149b809c6635874247680
j.schoenau <j.schoenau@fz-juelich.de>
2023-05-08 22:26:53 +0200

    update running instr

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ from src import MinimalRechner as mr
 
 cwd = Path(__file__).parent.resolve()
 in_folder = cwd.joinpath("files/in")
-#in_file = in_folder.joinpath("ihk.in")
+in_file = in_folder.joinpath("ihk.in")
 #in_file = in_folder.joinpath("redDuplikate.in")
 #in_file = in_folder.joinpath("redBahnhoefe.in")
-in_file = in_folder.joinpath("redZugverbindungen.in")
+#in_file = in_folder.joinpath("redZugverbindungen.in")
 
 #io = iom.IOManager()
 #io.schreibeAusgabe( io.verbindungZuKnoten( ["A", "B", "C", "D", "E"] ) )
